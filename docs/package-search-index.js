d5e832b28bcc50776383dc71fff50cd549775684
TTNO1 <65061246+TTNO1@users.noreply.github.com>
2026-07-26 16:49:30 -0400

    Update minor version, regen docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.ttno1.configvalidation"},{"l":"io.github.ttno1.configvalidation.defaultwrappers"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"configvalidation","l":"io.github.ttno1.configvalidation"},{"m":"configvalidation","l":"io.github.ttno1.configvalidation.defaultwrappers"}];updateSearchResults();
